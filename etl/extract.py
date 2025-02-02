ee13bc69df372163e70dce1a9c097bd486e0363f
ganjela <nikaganjelashvili@gmail.com>
2025-02-02 18:37:12 +0400

    add type annotations to extract func

--- etl/extract.py
+++ etl/extract.py
@@ -1,17 +1,21 @@
 import requests
+from typing import Dict, Any
 
-def extract_opensea_collections(api_key, chain = "ethereum"):
-    url = "https://api.opensea.io/api/v2/collections"
-    params = {"chain": chain}
+def extract_opensea_collections(api_key: str, chain: str = "ethereum") -> Dict[str, Any]:
+    url: str = "https://api.opensea.io/api/v2/collections"
+    params: Dict[str, str] = {"chain": chain}
 
-    headers = {
+    headers: Dict[str, str] = {
         "Accept": "application/json",
         "X-API-KEY": api_key
     }
-    response = requests.get(url, headers=headers, params=params)
+
+    response: requests.Response = requests.get(url, headers=headers, params=params)
+
     if response.status_code == 200:
         return response.json()
     else:
         raise Exception(f"Failed to fetch data: {response.status_code}")
+
 
 
